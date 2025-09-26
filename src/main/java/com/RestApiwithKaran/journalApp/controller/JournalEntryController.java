91a54c946ed63de771a5b4163f96e3dfa992c564
Karan Singh <veerkaransinghmgm@gmail.com>
2025-09-26 19:51:58 -0400

    initial commit

--- src/main/java/com/RestApiwithKaran/journalApp/controller/JournalEntryController.java
+++ src/main/java/com/RestApiwithKaran/journalApp/controller/JournalEntryController.java
@@ -3,6 +3,7 @@ package com.RestApiwithKaran.journalApp.controller;
 import com.RestApiwithKaran.journalApp.entity.JournalEntry;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
+import org.yaml.snakeyaml.error.MarkedYAMLException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +12,6 @@ import java.util.Map;
 @RestController
 @RequestMapping("/journal")
 // adds mapping specifically to this class
-
 public class JournalEntryController {
 
     private Map<Long, JournalEntry> journalEntriesMap = new HashMap<>();
@@ -23,20 +23,37 @@ public class JournalEntryController {
     public List<JournalEntry> getAll(){ //list of journal entry
        /* journalEntriesMap.put(1L, new JournalEntry(1L, "First", "MyFirstJournal"));
         journalEntriesMap.put(2L, new JournalEntry(2L, "Second", "MySecondJournal"));
-        journalEntriesMap.put(3L, new JournalEntry(3L, "Third", "MyThirdJournal")); */
-
+        journalEntriesMap.put(3L, new JournalEntry(3L, "Third", "MyThirdJournal"));*/
 
         return new ArrayList<>(journalEntriesMap.values());
         // .values gives all the values in the map, ignoring id's
         // values() returns a Collection, not a List.
         // wrap it with new ArrayList<>(...) to convert it into a List
-
     }
 
     @PostMapping
-    public void createEntry(@RequestBody){
+    public boolean createEntry(@RequestBody JournalEntry myEntry) { // myEntry is an instance of journal entry
         //Hey Spring, please take the data from the request and turn it into a java object that i can use in my code
-
+        System.out.println("Create Entry method is called");
+        System.out.println("I am getting this data from postman - body" + myEntry.toString());
+        journalEntriesMap.put(myEntry.getId(), myEntry);
+        System.out.println("Data from the post request has been added to map");
+        return true;
+    }
+    @GetMapping("id/{myId}")// to get any specific id which we ask via postman
+    public JournalEntry getJournalEntryById(@PathVariable Long myId){
+        return journalEntriesMap.get(myId); //whatever id will be passed via url from postman, will go here in(myId)
     }
 
+    @DeleteMapping("id/{myId}")
+    public JournalEntry deleteJournalEntryById(@PathVariable Long myId){
+        return journalEntriesMap.remove(myId);
+    }
+
+    @PutMapping("/id/{id}")
+    public JournalEntry updateJournalById(@PathVariable Long id, @RequestBody JournalEntry myEntry){
+       return journalEntriesMap.put(id, myEntry);
+    }
 }
+
+
