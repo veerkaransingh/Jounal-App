0241db38aa6808aeaedee3e60087e779c8be71f2
Karan Singh <veerkaransinghmgm@gmail.com>
2025-09-29 20:38:17 -0400

    Service Class

--- src/main/java/com/RestApiwithKaran/journalApp/Service/JournalEntryService.java
+++ src/main/java/com/RestApiwithKaran/journalApp/Service/JournalEntryService.java
@@ -2,11 +2,13 @@ package com.RestApiwithKaran.journalApp.Service;
 
 import com.RestApiwithKaran.journalApp.Repository.JournalEntryRepository;
 import com.RestApiwithKaran.journalApp.entity.JournalEntry;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 // Service class holds all the logic
 // Controller will call the service
@@ -28,6 +30,15 @@ public class JournalEntryService {
     @GetMapping
     public List<JournalEntry> getAll(){
         return journalEntryRepository.findAll();
-
     }
+
+
+    public Optional<JournalEntry> findById(ObjectId id){ //Optional -- may contain some value or not
+        return journalEntryRepository.findById(id);
+    }
+    public void deleteById(ObjectId id){
+        journalEntryRepository.deleteById(id);
+    }
+
+
 }
