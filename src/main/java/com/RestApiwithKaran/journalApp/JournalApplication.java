298fff8e70012db5474d482cf1f61baa6b194a45
Karan Singh <veerkaransinghmgm@gmail.com>
2025-10-06 21:54:09 -0400

    Commit

--- src/main/java/com/RestApiwithKaran/journalApp/JournalApplication.java
+++ src/main/java/com/RestApiwithKaran/journalApp/JournalApplication.java
@@ -2,8 +2,10 @@ package com.RestApiwithKaran.journalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement // now every method with @Transactional annotation will
 public class JournalApplication {
 
 	public static void main(String[] args) {
