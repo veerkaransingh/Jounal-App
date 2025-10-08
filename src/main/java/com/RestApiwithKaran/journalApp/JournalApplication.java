dd07ef5f8e0b626b1d77a42a48bf579454e1e25f
Karan Singh <veerkaransinghmgm@gmail.com>
2025-10-07 22:23:07 -0400

    Commit

--- src/main/java/com/RestApiwithKaran/journalApp/JournalApplication.java
+++ src/main/java/com/RestApiwithKaran/journalApp/JournalApplication.java
@@ -2,6 +2,10 @@ package com.RestApiwithKaran.journalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
@@ -12,5 +16,13 @@ public class JournalApplication {
 		SpringApplication.run(JournalApplication.class, args);
 	}
 
+	@Bean
+	PlatformTransactionManager add(MongoDatabaseFactory dbfactory){
+		return new MongoTransactionManager(dbfactory);
+	}
 }
+
+//PlatformTransactionManager - An interface
+//MongoTransactionManager -- Implementation of above interface
+
 
