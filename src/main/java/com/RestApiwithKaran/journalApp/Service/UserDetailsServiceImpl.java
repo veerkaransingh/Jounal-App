91ccbac7c5ae85625627d865cf9e7a11667f4574
Karan Singh <veerkaransinghmgm@gmail.com>
2025-10-13 00:26:26 -0400

    Commit

--- src/main/java/com/RestApiwithKaran/journalApp/Service/UserDetailsServiceImpl.java
+++ src/main/java/com/RestApiwithKaran/journalApp/Service/UserDetailsServiceImpl.java
@@ -9,21 +9,26 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 @Component
- public class UserDetailsServiceImpl implements UserDetailsService {
+ public class UserDetailsServiceImpl implements UserDetailsService { //this(UserDetailsService) is the class Spring Security calls whenever a user tries to log in
 
     @Autowired
     private  UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUserName(username);
+        //Spring Security receives the username (karan123) from the login form or Basic Auth header.
+        User user = userRepository.findByUserName(username); //You look up that user(karan123) in MongoDB
         if(user != null){
             UserDetails userDetails = org.springframework.security.core.userdetails.User.builder()
-                    .username(user.getUserName())
-                    .password(user.getPassword())
-                    .roles(user.getRoles().toArray(new String[0]))
-                    .build();
-            return userDetails;
+                    /*
+                    you’re using a built-in helper class called User (from Spring Security, not your own entity).
+                    It helps you build a UserDetails object, which Spring Security understands
+                     */
+                    .username(user.getUserName()) //Set username for Spring -- "karan123"
+                    .password(user.getPassword())  //Set encoded password --   $2a$10$ABC123...
+                    .roles(user.getRoles().toArray(new String[0])) //Give roles/permissions  -- ["USER"]
+                    .build(); //Create final UserDetails object ready for spring
+            return userDetails; //Give this object to Spring ---used for authentication
         }
         throw new UsernameNotFoundException("User not found with username: "+ username);
     }
