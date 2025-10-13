18ad4f10e0ea5318a080c65a437d92cd591afc7a
Karan Singh <veerkaransinghmgm@gmail.com>
2025-10-13 00:26:08 -0400

    Commit

--- src/main/java/com/RestApiwithKaran/journalApp/controller/PublicController.java
+++ src/main/java/com/RestApiwithKaran/journalApp/controller/PublicController.java
@@ -1,18 +1,30 @@
 package com.RestApiwithKaran.journalApp.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.RestApiwithKaran.journalApp.Service.UserService;
+import com.RestApiwithKaran.journalApp.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController // this class became a component with additional functionality
 //whatever endpoints or URL paths will be there, whatever they will return, will be automatically convert to JSON format
-public class HealthCheck {
+@RequestMapping("/public")
+public class PublicController {
 
+
+    @Autowired
+    private UserService userService;
     //We need to map this function to a path via @GetMapping
 
     @GetMapping("/health-check")
     public String healthCheck(){ //this function will get mapped to /health-check endpoint
         // @GetMapping simply means the call will be of get type
         return "Ok";
+    }
+
+    @PostMapping("/create-user")
+    public void createUser(@RequestBody User user){
+        userService.saveEntry(user);
+
     }
 }
