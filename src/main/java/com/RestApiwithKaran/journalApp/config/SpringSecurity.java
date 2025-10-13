fd8202d205dc39073153ef416277ea0556c6a6e2
Karan Singh <veerkaransinghmgm@gmail.com>
2025-10-13 00:26:16 -0400

    Commit

--- src/main/java/com/RestApiwithKaran/journalApp/config/SpringSecurity.java
+++ src/main/java/com/RestApiwithKaran/journalApp/config/SpringSecurity.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -24,18 +25,41 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter {
         http is an instance of class HttpSecurity
         */
         http.authorizeRequests()
-                    .antMatchers("/journal/**").authenticated() //permits all requests to hello without authentication
-                    .anyRequest().permitAll() // any other requests need to be authenticated and a form-based login will be used
-                //we can access /hello requests without any authentication but if we try to access any other requests, we will be redirected to a login form
+                    .antMatchers("/journal/**","/user/**").authenticated() //needs authenticated
+                    .anyRequest().permitAll() // rest all are permitted
                 .and()
-                .httpBasic();
-        http.csrf().disable();
+                .httpBasic();// no form based login, basic auth for postman
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf().disable();
+
+        //Don’t create sessions.
+        //Expect every request to include its own credentials (stateless).
+        //Disable CSRF because we’re building an API, not a cookie-based site.
+
+
+        /*
+        CSRF (Cross-Site Request Forgery) is a security protection that makes sense for web forms with sessions,
+         but it breaks APIs that don’t use cookies.
+        By disabling it, you’re saying:
+        “I’m building a REST API (stateless), not a browser form app. So CSRF tokens aren’t needed.”
+        Without disabling CSRF, your POST, PUT, and DELETE requests would get blocked (403 Forbidden) when testing from Postman or React frontend.
+         */
     }
 
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        /*
+         Think of AuthenticationManagerBuilder as a builder tool for telling Spring:
+        “Hey, this is where and how you should find users when they try to log in.”
+         So here, We are teaching spring that : where users come from (MongoDB), how passwords are checked (BCrypt)
+         */
+
+
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        //Tells Spring to fetch user details from MongoDB via service we created.
+        // then... Defines how to verify passwords
+        //Uses BCrypt to compare entered vs stored password
         super.configure(auth);
     }
 
