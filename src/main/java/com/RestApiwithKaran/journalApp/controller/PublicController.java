9585225cfb01e78b97fa792b9ef1a7785571a775
Karan Singh <veerkaransinghmgm@gmail.com>
2025-10-18 20:39:30 -0400

    Commit

--- src/main/java/com/RestApiwithKaran/journalApp/controller/PublicController.java
+++ src/main/java/com/RestApiwithKaran/journalApp/controller/PublicController.java
@@ -24,7 +24,7 @@ public class   PublicController {
 
     @PostMapping("/create-user")
     public void createUser(@RequestBody User user){
-        userService.saveEntry(user);
+        userService.saveNewUser(user);
 
     }
 }
