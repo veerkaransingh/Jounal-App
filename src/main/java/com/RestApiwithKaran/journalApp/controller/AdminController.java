d864b1d4502994ce857612b5efd3fa09cc424cbe
Karan Singh <veerkaransinghmgm@gmail.com>
2025-10-25 18:31:52 -0400

    Commit

---
+++ src/main/java/com/RestApiwithKaran/journalApp/controller/AdminController.java
@@ -0,0 +1,30 @@
+package com.RestApiwithKaran.journalApp.controller;
+
+
+import com.RestApiwithKaran.journalApp.Service.UserService;
+import com.RestApiwithKaran.journalApp.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/all-users")
+    public ResponseEntity<?> getAllUsers(){
+        List<User> all = userService.getAll();
+        if(all != null && !all.isEmpty()){
+            return new ResponseEntity<>(all, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+    }
+}
