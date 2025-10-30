39d3e9985108f7d99f7dea59d168ccfe3f6b2f44
Karan Singh <veerkaransinghmgm@gmail.com>
2025-10-30 16:57:18 -0400

    Commit

--- src/main/java/com/RestApiwithKaran/journalApp/controller/AdminController.java
+++ src/main/java/com/RestApiwithKaran/journalApp/controller/AdminController.java
@@ -6,9 +6,7 @@ import com.RestApiwithKaran.journalApp.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +24,9 @@ public class AdminController {
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 
+    }
+    @PostMapping("/create-admin-user")
+    public void createUser(@RequestBody User user){
+        userService.saveAdmin(user);
     }
 }
